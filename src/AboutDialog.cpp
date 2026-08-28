9ed20dff7075c4a35bf0144c8c59f3d36fe64178
Stanley Lloyd <131160519+StanleyLl0yd@users.noreply.github.com>
2026-08-28 18:13:37 +0300

    Modernize About dialog implementation

--- src/AboutDialog.cpp
+++ src/AboutDialog.cpp
@@ -5,6 +5,7 @@
 #include <commctrl.h>
 #include <shellapi.h>
 
+#include <bit>
 #include <string>
 
 namespace Everon {
@@ -16,44 +17,50 @@ constexpr wchar_t kRepositoryLinkText[] =
     L"<a href=\"https://github.com/StanleyLl0yd/everon\">github.com/StanleyLl0yd/everon</a>";
 
 void OpenRepository(HWND dialog, const wchar_t* url) {
+    using enum StringID;
+
     const HINSTANCE result = ShellExecuteW(dialog, L"open", url, nullptr, nullptr, SW_SHOWNORMAL);
-    if (reinterpret_cast<INT_PTR>(result) <= 32) {
+    if (std::bit_cast<INT_PTR>(result) <= 32) {
         const auto& loc = Localization::Instance();
-        MessageBoxW(dialog, url, loc.GetString(StringID::AboutTitle), MB_OK | MB_ICONINFORMATION);
+        MessageBoxW(dialog, url, loc.GetString(AboutTitle), MB_OK | MB_ICONINFORMATION);
     }
 }
 
 INT_PTR CALLBACK AboutDialogProc(HWND dialog, UINT message, WPARAM wParam, LPARAM lParam) {
+    using enum StringID;
+
     const auto& loc = Localization::Instance();
 
     switch (message) {
         case WM_INITDIALOG:
-            SetWindowTextW(dialog, loc.GetString(StringID::AboutTitle));
-            SetDlgItemTextW(dialog, IDC_ABOUT_VERSION, loc.GetString(StringID::AboutVersion));
-            SetDlgItemTextW(dialog, IDC_ABOUT_TAGLINE, loc.GetString(StringID::AboutTagline));
+            SetWindowTextW(dialog, loc.GetString(AboutTitle));
+            SetDlgItemTextW(dialog, IDC_ABOUT_VERSION, loc.GetString(AboutVersion));
+            SetDlgItemTextW(dialog, IDC_ABOUT_TAGLINE, loc.GetString(AboutTagline));
             SetDlgItemTextW(dialog, IDC_ABOUT_AUTHOR, L"Stanley Lloyd");
-            SetDlgItemTextW(dialog, IDC_ABOUT_LICENSE, loc.GetString(StringID::AboutLicense));
+            SetDlgItemTextW(dialog, IDC_ABOUT_LICENSE, loc.GetString(AboutLicense));
             SetDlgItemTextW(dialog, IDC_ABOUT_LINK, kRepositoryLinkText);
-            SetDlgItemTextW(dialog, IDOK, loc.GetString(StringID::ButtonOK));
+            SetDlgItemTextW(dialog, IDOK, loc.GetString(ButtonOK));
             return TRUE;
 
-        case WM_NOTIFY: {
-            const auto* header = reinterpret_cast<const NMHDR*>(lParam);
-            if (header && header->idFrom == IDC_ABOUT_LINK &&
+        case WM_NOTIFY:
+            if (const auto* header = std::bit_cast<const NMHDR*>(lParam);
+                header && header->idFrom == IDC_ABOUT_LINK &&
                 (header->code == NM_CLICK || header->code == NM_RETURN)) {
-                const auto* link = reinterpret_cast<const NMLINK*>(lParam);
+                const auto* link = std::bit_cast<const NMLINK*>(lParam);
                 const wchar_t* url = link->item.szUrl[0] ? link->item.szUrl : kRepositoryUrl;
                 OpenRepository(dialog, url);
                 return TRUE;
             }
             break;
-        }
 
         case WM_COMMAND:
             if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL) {
                 EndDialog(dialog, LOWORD(wParam));
                 return TRUE;
             }
+            break;
+
+        default:
             break;
     }
 
@@ -63,6 +70,8 @@ INT_PTR CALLBACK AboutDialogProc(HWND dialog, UINT message, WPARAM wParam, LPARA
 }
 
 void ShowAboutDialog(HINSTANCE instance, HWND parent) {
+    using enum StringID;
+
     const INT_PTR result = DialogBoxParamW(instance,
                                            MAKEINTRESOURCEW(IDD_ABOUT),
                                            parent,
@@ -70,15 +79,15 @@ void ShowAboutDialog(HINSTANCE instance, HWND parent) {
                                            0);
     if (result == -1) {
         const auto& loc = Localization::Instance();
-        std::wstring message = loc.GetString(StringID::AboutVersion);
+        std::wstring message = loc.GetString(AboutVersion);
         message += L"\n";
-        message += loc.GetString(StringID::AboutTagline);
+        message += loc.GetString(AboutTagline);
         message += L"\n\nStanley Lloyd\n";
-        message += loc.GetString(StringID::AboutLicense);
+        message += loc.GetString(AboutLicense);
         message += L"\n\n";
         message += kRepositoryUrl;
 
-        MessageBoxW(parent, message.c_str(), loc.GetString(StringID::AboutTitle),
+        MessageBoxW(parent, message.c_str(), loc.GetString(AboutTitle),
                     MB_OK | MB_ICONINFORMATION);
     }
 }
